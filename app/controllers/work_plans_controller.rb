304df64275c8798f4b5289726937c0d858c0ee85
Guenole Quideau <gqprof@protonmail.com>
2021-11-30 17:30:52 +0100

    New Create work_plan OK

---
+++ app/controllers/work_plans_controller.rb
@@ -0,0 +1,22 @@
+class WorkPlansController < ApplicationController
+  def new
+    @work_plan = WorkPlan.new
+    @students = Student.where(classroom: current_user.classrooms)
+  end
+
+  def create
+    @work_plan = WorkPlan.new(work_plan_params)
+    @work_plan.user = current_user
+    if @work_plan.save!
+      redirect_to work_plan_path(@work_plan)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def work_plan_params
+    params.require(:work_plan).permit(:name, :student_id)
+  end
+end
