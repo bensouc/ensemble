b562ea7da2202c40ec409727ff88e0c455df08fc
Guenole Quideau <gqprof@protonmail.com>
2021-12-02 13:30:00 +0100

    Create Work_plan Pb sur selection competence

--- app/controllers/work_plans_controller.rb
+++ app/controllers/work_plans_controller.rb
@@ -14,12 +14,16 @@ class WorkPlansController < ApplicationController
     @students = Student.where(classroom: current_user.classrooms)
 
     # generate the first work_plan_domain for new work_plan
-    @work_plan.work_plan_domains.new
+    @work_plan_domain = @work_plan.work_plan_domains.new
+
+    # generate the first work_plan_skill for first work_plan_domain
+    @work_plan_domain.work_plan_skills.new
   end
 
   def create
     @work_plan = WorkPlan.new(work_plan_params)
     @work_plan.user = current_user
+    raise
     if @work_plan.save
       redirect_to work_plan_path(@work_plan)
     else
@@ -31,6 +35,7 @@ class WorkPlansController < ApplicationController
 
   def work_plan_params
     params.require(:work_plan).permit(:name, :student_id, :start_date, :end_date,
-                                      work_plan_domains_attributes: %i[domain level])
+                                      work_plan_domains_attributes: %i[domain level],
+                                      work_plan_skills_attributes: :name)
   end
 end
