430438cf206115b49fec75afe87894fdb033de68
benoit moret <benoit.moret@gmail.com>
2022-01-24 14:04:04 +0100

    add alertMsg to challenge name edtion in show

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -2,8 +2,11 @@ class ChallengesController < ApplicationController
   def update
     @work_plan_skill = WorkPlanSkill.find(params[:work_plan_skill_id])
     @challenge = Challenge.find(params[:id])
-    @challenge.update(challenge_params)
-        redirect_to work_plan_path(@work_plan_skill.work_plan_domain.work_plan, anchor: helpers.dom_id(@challenge))
+    if @challenge.update(challenge_params)
+      redirect_to work_plan_path(@work_plan_skill.work_plan_domain.work_plan, anchor: helpers.dom_id(@challenge)), notice: 'Excercice Sauvegardé'
+    else
+      redirect_to work_plan_path(@work_plan_skill.work_plan_domain.work_plan, anchor: helpers.dom_id(@challenge)), notice: "Sauvegarde échouée: #{@challenge.errors.messages}"
+    end
   end
 
   def challenge_params
