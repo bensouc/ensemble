4817b371204c44ea8cf4b5cfa9cc172bf0c7c8a3
benoit moret <benoit.moret@gmail.com>
2022-02-18 10:57:42 +0100

    WP header editing with JS working

--- app/javascript/controllers/edit_wpheader_controller.js
+++ app/javascript/controllers/edit_wpheader_controller.js
@@ -1,12 +1,13 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ['infos', 'form'];
+  static targets = ['button', 'form'];
   connect() {
     console.log('WP-edit connected');
   }
 
   displayForm() {
+    console.log('display toggle BMO');
     this.buttonTarget.classList.add('d-none');
     this.formTarget.classList.remove('d-none');
   }
