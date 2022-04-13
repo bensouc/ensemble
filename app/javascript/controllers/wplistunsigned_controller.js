a65be7fbd91ae2b1dc0ae19be110966a13681268
benoit moret <benoit.moret@gmail.com>
2022-04-13 15:04:10 +0200

    WP index V2 + JS

--- app/javascript/controllers/wplistunsigned_controller.js
+++ app/javascript/controllers/wplistunsigned_controller.js
@@ -1,15 +1,14 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ['wplist', 'folderopen', 'folderclosed'];
+  static targets = ['wplistus', 'folderopen', 'folderclosed'];
   connect() {
-    console.log('wp-by-student');
+    console.log('wplistus');
   }
 
   displayList() {
     this.folderopenTarget.classList.toggle('d-none');
     this.folderclosedTarget.classList.toggle('d-none');
-    this.wplistTarget.classList.toggle('d-none');
-
+    this.wplistusTarget.classList.toggle('d-none');
   }
 }
