3b9cc4fe12e51f1c97623e5a97802f5c417b5e2f
benoit moret <benoit.moret@gmail.com>
2022-04-18 18:35:13 +0200

    add remoe a student to a classroom

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,32 @@
+class StudentsController < ApplicationController
+  def new
+    @classroom = Classroom.find(classroom_params_id)
+    @student = Student.new(classroom_id: @classroom.id)
+  end
+
+  def create
+    student = {
+      first_name: params_student[:first_name],
+      classroom_id: params_student[:classroom].to_i,
+
+    }
+    @student = Student.create(student)
+    redirect_to classrooms_path
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to classrooms_path
+  end
+
+  private
+
+  def params_student
+    params.require(:student)
+  end
+
+  def classroom_params_id
+    params.require(:classroom_id)
+  end
+end
