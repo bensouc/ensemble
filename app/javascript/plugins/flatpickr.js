fdd8e1ced6ebcf2d5ae8742b51848b0a9dfe8d3b
benoit moret <benoit.moret@gmail.com>
2022-05-02 13:15:11 +0200

    update datepicker style-2

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,7 +3,7 @@ import { French } from "flatpickr/dist/l10n/fr.js"
 
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", { dateFormat: 'd/m/Y', locale: French,allowInput: true });
+  flatpickr(".datepicker", { dateFormat: 'd/m/Y', locale: French, allowInput: true });
   // flatpickr(".datepicker",{dateFormat: 'd/m/Y'})
 }
 
