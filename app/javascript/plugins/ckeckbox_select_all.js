b4c7459b6b601beba45923820819d76d96fca020
benoit moret <benoit.moret@gmail.com>
2022-07-21 12:12:53 +0200

    small update on cloning_scss

--- app/javascript/plugins/ckeckbox_select_all.js
+++
@@ -1,6 +0,0 @@
-$('[data-behaviour="select-all"][value=0]').on('change', function () {
-  var checked = this.checked;
-  $('[data-behaviour="select-all"]').each(function () {
-    $(this).prop('checked', checked);
-  });
-})
