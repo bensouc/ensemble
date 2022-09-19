a122ec4e49f4430843fda8b7792f66ad9b5d1bbd
benoit moret <benoit.moret@gmail.com>
2022-09-19 14:21:29 +0200

    eval mobile fonctionnelle

--- app/controllers/mobile/work_plans_controller.rb
+++ app/controllers/mobile/work_plans_controller.rb
@@ -5,4 +5,19 @@ class Mobile::WorkPlansController < ApplicationController
     @my_work_plans_unassigned = @my_work_plans.where(student: nil)
     @my_work_plans = @my_work_plans.where.not(student: nil).sort_by(&:student)
   end
+
+  def eval
+    # @belt = Belt::BELT_COLORS
+    @work_plan = WorkPlan.find(params[:id])
+    @domains = @work_plan.all_domains_from_work_plan
+    @previous = []
+    @wpds = WorkPlanDomain.where(work_plan: @work_plan)
+    @wpds.each do |wpd|
+      wpd.work_plan_skills.each do |wps|
+        # last_4_wps = WorkPlanSkill.where(student: @work_plan.student, skill: wps.skill_id).sort_by(&:created_at).reverse[1..3]
+        last_4_wps = WorkPlanSkill.last_4_wps(@work_plan, wps)
+        @previous << [wps.skill_id, last_4_wps]
+      end
+    end
+  end
 end
