54dd7bfa29a97cfd774a04fa63e86050938a41c5
benoit moret <benoit.moret@gmail.com>
2022-11-06 15:39:52 +0100

    add classroom sharing to mobile

--- app/controllers/mobile/work_plans_controller.rb
+++ app/controllers/mobile/work_plans_controller.rb
@@ -1,9 +1,25 @@
 class Mobile::WorkPlansController < ApplicationController
   def index
-    @my_classrooms = Classroom.where(user: current_user)
-    @my_work_plans = WorkPlan.where(user: current_user).order(created_at: :DESC) # .sort_by(&:student)
+    # @my_classrooms = Classroom.where(user: current_user)
+    # @my_work_plans = WorkPlan.where(user: current_user).order(created_at: :DESC) # .sort_by(&:student)
+    # @my_work_plans_unassigned = @my_work_plans.where(student: nil)
+    # @my_work_plans = @my_work_plans.where.not(student: nil).sort_by(&:student)
+    @shared_classrooms = current_user.shared_classrooms
+    shared_classrooms = @shared_classrooms.map(&:classroom)
+    @my_classrooms = (current_user.classrooms + shared_classrooms).sort_by(&:created_at)
+    shared_work_plans = []
+    shared_classrooms.each do |classroom|
+      classroom.students.each do |shared_student|
+        WorkPlan.where(student: shared_student).order(created_at: :DESC).each do |work_plan|
+          shared_work_plans << work_plan
+        end
+      end
+    end
+    @my_work_plans = WorkPlan.where(user: current_user).order(created_at: :DESC)
+    # .sort_by(&:student)
     @my_work_plans_unassigned = @my_work_plans.where(student: nil)
     @my_work_plans = @my_work_plans.where.not(student: nil).sort_by(&:student)
+    @my_work_plans += shared_work_plans
   end
 
   def eval
