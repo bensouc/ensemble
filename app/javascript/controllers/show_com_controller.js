e8f036149c4c7f35f3997efd8e5d5fff641826b9
benoit moret <benoit.moret@gmail.com>
2022-12-13 11:54:00 +0100

    edit info dashboard

--- app/javascript/controllers/show_com_controller.js
+++ app/javascript/controllers/show_com_controller.js
@@ -8,9 +8,10 @@ export default class extends Controller {
 
   displayCom() {
     swal({
-      title: "Ensemble change d'adresse !! \n www.app-ensemble.fr",
-      text: "\n Depuis hier, l'application est disponible à cette nouvelle adresse \n Penser à mettre à jour vos liens, et notamment sur le mobile. \n \n L'ancienne adresse ne sera plus disponible à partir du 28 novembre",
-      icon: "info"
+      title: "Ensemble Vous souhaite de belles vacances !! \n",
+      text:
+        "Nous restons ouverts pendant les fêtes, n'hesitez pas, " +
+        'contact@vroadstudio.fr'
     });
   }
 }
