29de6aa8712d39c23d0e95ab4b6e762cac6731d2
benoit moret <benoit.moret@gmail.com>
2023-01-09 12:28:58 +0100

    Edit belt with edit page: chnage validateion date ans destroy-reday for skill addition

--- app/controllers/belts_controller.rb
+++ app/controllers/belts_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class BeltsController < ApplicationController
+  def edit
+    @belt = Belt.find(params[:id])
+    @skills = @belt.all_skills
+  end
+
   def create
     @belt = Belt.create(new_belt_params)
     @belt.completed = true
@@ -9,6 +14,16 @@ class BeltsController < ApplicationController
     @belt.save
 
     redirect_to student_path(@belt.student)
+  end
+
+  def update
+    @belt = Belt.find(params[:id])
+    @belt.validated_date = new_belt_params[:validated_date]
+    if @belt.save
+      redirect_to student_path(@belt.student)
+    else
+      redirect_to :edit
+    end
   end
 
   def destroy
@@ -21,6 +36,6 @@ class BeltsController < ApplicationController
   private
 
   def new_belt_params
-  params.require(:belt).permit( :grade, :domain, :level, :grade)
+    params.require(:belt).permit(:grade, :domain, :level, :grade, :validated_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
     post "", to: "work_plans#auto_new_wp", as: :auto_new_wp
   end
 
-  resources :belts, only: [:destroy]
+  resources :belts, only: [:destroy, :edit, :update]
 
   # route for tab editing
   resources :tables, only: [:show, :create, :update]
