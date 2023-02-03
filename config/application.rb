17fe06501fc322be4d526236a96597fec0829601
benoit moret <benoit.moret@gmail.com>
2023-02-03 09:34:37 +0100

    review i18 locales

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Ensemble
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.i18n.available_locales = [:fr, :en]
+    config.i18n.load_path += Dir[Rails.root.join("my", "locales", "*.{rb,yml}")]
+    config.i18n.available_locales = [:fr]
+    config.i18n.default_locale = :fr
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
