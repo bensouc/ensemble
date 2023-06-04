9e8c52eff77db61f59036d10905aab0a0d5ac60e
benoitmoret <benoit.moret@gmail.com>
2023-06-04 23:38:54 +0200

    s

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -101,7 +101,6 @@ class ClassroomsController < ApplicationController
       # all_completed_belts = Belt.includes([:student]).where(student: students_list, domain: domain, completed: true)
       workbook.add_worksheet(name: "#{domain.capitalize}") do |sheet|
         sheet.add_row header.flatten
-        raise
         @skills.select { |skill| skill.domain == domain }.sort_by { |skill| [skill.level, skill.id] }.each do |skill|
           sheet.add_row create_result_row(skill, students_list).flatten
         end
