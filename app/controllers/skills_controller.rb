fb98469d6dcd9e24f7df2b554d63bd3cc9a34ff0
benoitmoret <benoit.moret@gmail.com>
2023-08-14 17:02:02 +0200

    xslx for skill by level

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -6,6 +6,7 @@ class SkillsController < ApplicationController
   def index
     @grades = current_user.classroom_grades
     query = params[:grade]
+    @school = current_user.school
     @grade = if query.nil?
         @grades.first  # gérer la query later ici
       else
@@ -14,6 +15,13 @@ class SkillsController < ApplicationController
     @skills = Skill.includes([:challenges]).for_school(current_user.school)
     @are_special_domains = current_user.school.id == 1
     @skills = @skills.select { |skill| skill.grade == @grade }
+    respond_to do |format|
+      format.html
+      format.xlsx do
+        response.headers["Content-Disposition"] = 'attachment; filename="my_new_filename.xlsx"'
+        generate_xlsx_file
+      end
+    end
     # for production => "A fournier == "
     # @are_special_domains = current_user.school.id == 1
   end
@@ -65,5 +73,30 @@ class SkillsController < ApplicationController
 
   def skill_params
     params.require(:skill).permit(:name, :grade, :symbol, :level, :domain)
+  end
+
+  # XLSX GENERATION
+  def generate_xlsx_file
+  package = Axlsx::Package.new
+  workbook = package.workbook
+  header = %w[Ceinture Compétences]
+  # students_list = @classroom.students.sort_by { |student| student.first_name.downcase }
+  # header << students_list.map { |student| student.first_name.capitalize }
+  # create a tab for each domain
+  WorkPlanDomain::DOMAINS[@grade].each do |domain|
+    # all_completed_belts = Belt.includes([:student]).where(student: students_list, domain: domain, completed: true)
+    workbook.add_worksheet(name: domain.capitalize.to_s) do |sheet|
+      sheet.add_row header.flatten
+      @skills.select { |skill| skill.domain == domain }.sort_by { |skill| [skill.level, skill.id] }.each do |skill|
+        sheet.add_row [skill.specials? ? "" : Belt::BELT_COLORS[skill.level - 1], "#{skill.symbol} #{skill.name}"]
+      end
+    end
+  end
+    # Enregistrez le fichier XLSX dans un temp file et envoyez-le en tant que pièce jointe
+  temp_file = Tempfile.new("temp.xlsx")
+  package.serialize(temp_file.path)
+  send_file temp_file,
+            filename: "#{@school.name.upcase}_#{@grade}_compétences_#{Time.zone.today}.xlsx",
+            type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
   end
 end
