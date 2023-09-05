f9792fa7314f75e6083ccbac4fcdc1118e8025c8
benoit moret <benoit.moret@gmail.com>
2023-09-05 15:49:50 +0200

    back for school show ok

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,15 @@
+class SchoolsController < ApplicationController
+  before_action :set_school, only: %w[show]
+
+  def show
+  end
+
+
+  private
+
+  def set_school
+    @school = School.includes(:users).find(params[:id])
+    @classrooms = @school.classrooms
+    @students = @school.all_students_list
+  end
+end
--- app/models/school.rb
+++ app/models/school.rb
@@ -3,4 +3,8 @@ class School < ApplicationRecord
   has_many :users, through: :school_roles  # Une école a plusieurs utilisateurs à travers schoolRole
   has_many :classrooms, through: :users
   has_many :skills, dependent: :destroy
+
+  def all_students_list
+    classrooms.map {|classroom| classroom.students}.flatten
+  end
 end
