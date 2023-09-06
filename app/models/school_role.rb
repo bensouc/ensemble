ebaa9c43e9c50b13325f99cb76497d8c746a5a96
benoit moret <benoit.moret@gmail.com>
2023-09-06 11:11:14 +0200

    add user 4 school /block user creation page sign_up

--- app/models/school_role.rb
+++ app/models/school_role.rb
@@ -1,4 +1,8 @@
 class SchoolRole < ApplicationRecord
   belongs_to :user
   belongs_to :school
+
+  def super_teacher?
+    super_teacher
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
   root to: "pages#home"
   get "/dashboard", to: "dashboard#show"
 
