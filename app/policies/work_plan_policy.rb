b6a38ce78364fc4faed51d335384652380f07845
benoit moret <benoit.moret@gmail.com>
2023-09-07 16:36:10 +0200

    PUNDIT work_Plan OCntroler OK

---
+++ app/policies/work_plan_policy.rb
@@ -0,0 +1,35 @@
+class WorkPlanPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.includes([:student]).where(user:,
+        special_wps: false).order(created_at: :DESC)
+      end
+
+    end
+    def show?
+      user_is_owner_or_admin?
+    end
+    def create?
+      true
+    end
+
+    def update?
+      user_is_owner_or_admin?
+    end
+
+    def destroy?
+    user_is_owner_or_admin?
+    end
+
+    def evaluation?
+      user_is_owner_or_admin?
+    end
+
+    private
+
+    def user_is_owner_or_admin?
+      user.admin || record.user.school == user.school
+    end
+
+end
