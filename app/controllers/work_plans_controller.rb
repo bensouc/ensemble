dbf7cc08d0d844f660c46353b7dde8c58a94197e
benoit moret <benoit.moret@gmail.com>
2023-09-25 16:16:00 +0200

    update refonte size challenge et apdf

--- app/controllers/work_plans_controller.rb
+++ app/controllers/work_plans_controller.rb
@@ -113,6 +113,7 @@ class WorkPlansController < ApplicationController
       format.pdf do
         render pdf: "#{@work_plan.name} #{@work_plan.student.first_name unless @work_plan.student.nil?}",
                template: "pdf/show_print",
+               dpi: 380,
                formats: [:html],
                disposition: "attachment",
                encoding: "utf8", # a remettre pour lle DL auto des pdf
