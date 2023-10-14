3842b6af77c348f63b6b2f340b63f0ea35c99c76
benoit moret <benoit.moret@gmail.com>
2023-10-14 16:28:51 +0200

    update srtipe production id and key

--- app/controllers/stripe/checkouts_controller.rb
+++ app/controllers/stripe/checkouts_controller.rb
@@ -1,17 +1,16 @@
 class Stripe::CheckoutsController < ApplicationController
   def create_subscription_checkout
-  Stripe.api_key = ENV["STRIPE_API_KEY"]
-  session = Stripe::Checkout::Session.create({
-    success_url: "https://app-ensemble.fr/subscriptions/success",
-    line_items: [
-      { price: "price_1O0jx5D36LSTpclfjnLgQSRt", quantity: 2 },
-    ],
-    mode: "subscription",
-    # customer: current_user.school.stripe_customer_id,
-    customer_email: current_user.school.email,
-  })
-  authorize session
-  redirect_to session.url, allow_other_host: true
-end
-
+    Stripe.api_key = ENV["STRIPE_API_KEY"]
+    session = Stripe::Checkout::Session.create({
+      success_url: "https://app-ensemble.fr/subscriptions/success",
+      line_items: [
+        { price: "price_1O0jx5D36LSTpclfjnLgQSRt", quantity: 2 },
+      ],
+      mode: "subscription",
+      # customer: current_user.school.stripe_customer_id,
+      customer_email: current_user.school.email,
+    })
+    authorize session
+    redirect_to session.url, allow_other_host: true
+  end
 end
