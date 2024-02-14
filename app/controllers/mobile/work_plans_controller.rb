6c1c1b4eb82dfc203dd0656a773ffca4b8f2711d
benoit moret <benoit.moret@gmail.com>
2024-02-14 14:31:37 +0100

    mobile completed

--- app/controllers/mobile/work_plans_controller.rb
+++ app/controllers/mobile/work_plans_controller.rb
@@ -26,9 +26,10 @@ class Mobile::WorkPlansController < ApplicationController
     # @belt = Belt::BELT_COLORS
     @work_plan = WorkPlan.find(params[:id])
     authorize @work_plan
-    @domains = @work_plan.all_domains_from_work_plan
+    @domains = @work_plan.grade.domains
     @previous = []
-    @wpds = WorkPlanDomain.where(work_plan: @work_plan)
+    @student =@work_plan.student
+    @wpds = @work_plan.work_plan_domains
     @wpds.each do |wpd|
       wpd.work_plan_skills.each do |wps|
         # last_4_wps = WorkPlanSkill.where(student: @work_plan.student, skill: wps.skill_id).sort_by(&:created_at).reverse[1..3]
