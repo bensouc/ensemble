aa8d1bf8df3c30a098a5a4239b4836a34f9098a7
benoit moret <benoit.moret@gmail.com>
2024-02-14 19:07:34 +0100

    edit domain sOK

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -2,7 +2,7 @@
 
 class DomainsController < ApplicationController
   skip_after_action :verify_policy_scoped, only: [:index]
-  before_action :set_domain, only: [:edit, :destroy]
+  before_action :set_domain, only: [:edit, :destroy, :update]
 
   def index
     @grade = Grade.find(params[:grade_id])
@@ -17,6 +17,8 @@ class DomainsController < ApplicationController
   end
 
   def edit
+    authorize(@domain)
+    @grade = @domain.grade
   end
 
   def create
@@ -27,6 +29,18 @@ class DomainsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to grade_domains_path(@grade), notice: "Domaine Sauvegardé" }
       format.turbo_stream
+    end
+  end
+
+  def update
+    authorize @domain
+    if @domain.update(set_params)
+      respond_to do |format|
+        format.html { redirect_to domain_path(@domain), notice: "Domaine Sauvegardé" }
+        format.turbo_stream
+      end
+    else
+      redirect_to redirect_to grade_domains_path(@domain.grade), notice: "Sauvegarde échouée "
     end
   end
 
