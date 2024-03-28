307de76325bae32c927698bcb71c4f5109fc9f92
benoit moret <benoit.moret@gmail.com>
2024-03-28 10:19:33 +0100

    upgrade RSPEC => 68%

--- spec/requests/schools_spec.rb
+++ spec/requests/schools_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Schools", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /show" do
+    it "goes to the school show" do
+      school = School.last
+      get "/schools/#{school.id}"
+      expect(response).to have_http_status(:found)
+    end
+
   end
 end
--- spec/requests/skills_spec.rb
+++ spec/requests/skills_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Skills", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "returns a success response" do
+      get "/skills"
+      expect(response).to have_http_status(:found)
+    end
   end
 end
