172ec090cc1fed9eab6c584246f47a1b6ede43b3
benoit moret <benoit.moret@gmail.com>
2024-06-24 15:55:13 +0200

    update tutaux links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,17 +6,18 @@ module ApplicationHelper
   def get_tuto_links(controller_name)
     case controller_name
     when "skills"
-      "https://www.notion.so/G-rer-les-comp-tences-du-groupe-cole-234534e947734471ab36a40e35433943?pvs=4"
+      "https://vroadstudio.notion.site/G-rer-les-comp-tences-du-groupe-cole-234534e947734471ab36a40e35433943?pvs=4"
     when "challenges"
-      "https://www.notion.so/Les-Exercices-39c32f7477a248c2889b93d055df67b8?pvs=4"
+      "https://vroadstudio.notion.site/Les-Exercices-39c32f7477a248c2889b93d055df67b8?pvs=4"
     when "classrooms"
-      "https://www.notion.so/G-rer-vos-classes-4f58dcf62df44cdca2b48054df551256?pvs=4"
+      "https://vroadstudio.notion.site/G-rer-vos-classes-4f58dcf62df44cdca2b48054df551256?pvs=4"
     when "work_plans"
-      "https://www.notion.so/Vos-Plans-de-Travail-8eea69c2f9be452e89e528a7e3f56f0d?pvs=4"
+      "https://vroadstudio.notion.site/Vos-Plans-de-Travail-8eea69c2f9be452e89e528a7e3f56f0d?pvs=4"
+
     when "subscriptions"
-      "https://https://vroadstudio.notion.site/Ensemble-Les-Tutos-1bb72deab51d43898dd2bdcec25ec098/S-abonner-9d9b895b3e9042648df01943941dcb07?pvs=4"
+      "https://vroadstudio.notion.site/Ensemble-Les-Tutos-1bb72deab51d43898dd2bdcec25ec098/S-abonner-9d9b895b3e9042648df01943941dcb07?pvs=4"
     else
-      "https://https://vroadstudio.notion.site/Ensemble-Les-Tutos-1bb72deab51d43898dd2bdcec25ec098/Ensemble-Les-Tutos-1bb72deab51d43898dd2bdcec25ec098"
+      "https://vroadstudio.notion.site/Ensemble-Les-Tutos-1bb72deab51d43898dd2bdcec25ec098/Ensemble-Les-Tutos-1bb72deab51d43898dd2bdcec25ec098"
     end
   end
 
