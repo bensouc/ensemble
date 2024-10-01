13f3475eb0196e2f49466e8e5c3ca6f0b8f5838e
benoit moret <benoit.moret@gmail.com>
2024-10-01 15:20:06 +0200

    update unreda message

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -14,21 +14,11 @@ RSpec.describe Message, type: :model do
     it { is_expected.to validate_presence_of(:content) }
   end
 
-  describe "#act_as_read" do
-    let!(:conversation) { create(:conversation, conversation_type: "group") }
-    let!(:user2) { create(:user) }
-    let!(:message) { create(:message, user: user2, conversation: conversation) }
-    context "when the message created is unread" do
-      it "returns false" do
-        conversation.users << user2
-        expect(user.have_read?(message)).to be false
-      end
-    end
-    context "when the message is read" do
-      before { message.mark_as_read! for: user }
-      it "returns true" do
-        expect(user.have_read?(message)).to be true
-      end
-    end
-  end
+  # describe "#mark_as_read!" do
+  #   let!(:message2) { create(:message, user: user, conversation: conversation) }
+  #   it "marks the message as read for the user" do
+  #     message2.mark_as_read!(user)
+  #     expect(message2.read_by?(user)).to be true
+  #   end
+  # end
 end
