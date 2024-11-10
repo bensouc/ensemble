bb25d7aa391b401c9826498858adadf59948950a
benoit moret <benoit.moret@gmail.com>
2024-11-10 23:53:14 +0100

    add cocmment for job

--- app/jobs/generate_results_classroom_pdfs_job.rb
+++ app/jobs/generate_results_classroom_pdfs_job.rb
@@ -7,9 +7,10 @@ class GenerateResultsClassroomPdfsJob < ApplicationJob
     # Define the name of the zip file
     zipfile_name = "classroom_#{classroom.id}_students_pdfs.zip"
     temp_file = Tempfile.new(zipfile_name)
-
+    begin
     # Create a new zip file
     Zip::OutputStream.open(temp_file) { |zos| } # Create a new zip file
+    Rails.logger.info("Created empty zip file at #{temp_file.path}")
     # Open the zip file and add a PDF for each student
     Zip::File.open(temp_file.path, Zip::File::CREATE) do |zipfile|
       students.each do |student|
@@ -17,13 +18,23 @@ class GenerateResultsClassroomPdfsJob < ApplicationJob
         zipfile.get_output_stream("Progression de #{student.first_name}_#{Time.current.strftime("_%Y_%m_%d")}.pdf") { |f| f.write(pdf) }
       end
     end
+      Rails.logger.info("Added PDFs to zip file for classroom #{classroom.id}")
 
-    # Enregistrer le fichier ZIP dans public/downloads
-    FileUtils.mv(temp_file.path, Rails.root.join("tmp", zipfile_name))
+      # Enregistrer le fichier ZIP dans public/downloads
+      destination_path = Rails.root.join("tmp", zipfile_name)
+      FileUtils.mv(temp_file.path, destination_path)
+      Rails.logger.info("Moved zip file to #{destination_path}")
 
-    # Ensure the temporary file is closed and unlinked
-    # # même si une exception est levée est levée dans le bloc begin
-    # temp_file.close
-    # temp_file.unlink
+      # Vérifier que le fichier existe dans tmp
+      if File.exist?(destination_path)
+        Rails.logger.info("Zip file successfully created and moved to #{destination_path}")
+      else
+        Rails.logger.error("Failed to move zip file to #{destination_path}")
+      end
+    ensure
+      # Ensure the temporary file is closed and unlinked
+      temp_file.close
+      temp_file.unlink
+    end
   end
 end
