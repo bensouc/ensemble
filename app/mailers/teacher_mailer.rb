91a8b9f511a724915d3ba1b38e813c56f8758b4f
benoit moret <benoit.moret@gmail.com>
2024-11-13 16:10:01 +0100

    add datatime in zipfile

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -1,9 +1,9 @@
 class TeacherMailer < ApplicationMailer
   include ApplicationHelper
-  
+
   def send_classroom_results_email(user, classroom, zipfile)
     @teacher = user
-    attachments[sanitize_filename("classe_ #{classroom.safe_name}_students_pdfs")+'.zip'] = File.read(zipfile)
+    attachments[sanitize_filename("resultats_#{Time.current.strftime("_%Y_%m_%d_")}classe_ #{classroom.safe_name}_students_pdfs")+'.zip'] = File.read(zipfile)
     mail(to: @teacher.email, subject: 'Ensemble: Vos résultats sont prêts !')
   end
 end
