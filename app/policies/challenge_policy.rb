d4076ef665e138d13beb20d744680ea0725f5d52
benoit moret <benoit.moret@gmail.com>
2025-07-11 13:05:56 +0200

    change ce1 valuz for special bekt validation + allow challenge destroy even you are not the creator

--- app/policies/challenge_policy.rb
+++ app/policies/challenge_policy.rb
@@ -1,12 +1,13 @@
 class ChallengePolicy < ApplicationPolicy
   class Scope < Scope
-    def resolve()
+    def resolve
       # scope.includes([:skill, :rich_text_content]).same_school(user).all
       # school = user.school
       # skills = school.skills
       # binding.pry
       # scope.includes([:skill, :rich_text_content]).where(skills: skills)
-      scope.includes([:skill, :rich_text_content, :work_plan_skills]).joins(:skill).where(skills: { school: user.school })
+      scope.includes([:skill, :rich_text_content,
+                      :work_plan_skills]).joins(:skill).where(skills: { school: user.school })
     end
   end
 
@@ -26,9 +27,6 @@ class ChallengePolicy < ApplicationPolicy
     user_is_owner_or_admin?
   end
 
-  def destroy?
-    user_is_owner_or_admin? || record.user.admin?
-  end
 
   def clone?
     user_is_owner_or_admin?
@@ -38,9 +36,25 @@ class ChallengePolicy < ApplicationPolicy
     user_is_owner_or_admin?
   end
 
+  def destroy?
+    user_can_destroy? && challenge_not_used?
+  end
+  
+  def can_show_delete_button?
+    user_can_destroy? && challenge_not_used?
+  end
+
   private
 
   def user_is_owner_or_admin?
     user.admin || record.skill.school == user.school
+  end
+
+  def user_can_destroy?
+    record.user == user || user.admin? || record.user.admin?
+  end
+
+  def challenge_not_used?
+    record.work_plan_skills.empty?
   end
 end
